4de9ee11e7e05cfd81c742270b7a642fb3e641ec
andrenascimentobarbosa <andrebarbosa2024@gmail.com>
2026-02-19 07:07:38 -0300

    update

---
+++ configfiles/.switchaudio.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+
+file='/tmp/audio.txt'
+
+
+
+
+devs=('pactl set-default-sink alsa_output.pci-0000_00_1f.3.analog-stereo'
+        'pactl set-default-sink alsa_output.usb-Yealink_Yealink_UH34_SE_300078F020112985-00.analog-stereo')
+
+
+
+
+if [[ -f "$file" ]]; then
+    current=$(cat "$file")
+else
+    current=-1
+fi
+
+next=$(( (current + 1) % ${#devs[@]} ))
+
+eval "${devs[$next]}"
+
+echo "$next" > "$file"
+
+
+
